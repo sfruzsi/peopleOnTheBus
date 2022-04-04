e4767aaa3fed3c8680ffcb2c041fb4119f6264c9
Fruzsina Strbik <fruzsina.strbik1@ibm.com>
2022-04-04 11:42:30 +0200

    feat: test 2 fail

--- src/peopleOnTheBus.test.js
+++ src/peopleOnTheBus.test.js
@@ -3,4 +3,7 @@ describe('User story 1', () => {
   it('Given bus with stop [0,0] When calculating Then reuslt should be 0', () => {
     expect(peopleOnTheBus([[0,0]])).toBe(0);
   });
+  it('Given bus with stop [1,0] When calculating Then reuslt should be 1', () => {
+    expect(peopleOnTheBus([[1,0]])).toBe(1);
+  });
 });
