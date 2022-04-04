628a169f789cd91d007e83d63316606203a34871
Fruzsina Strbik <fruzsina.strbik1@ibm.com>
2022-04-04 11:56:04 +0200

    feat: 2.2 test GREEN

--- src/peopleOnTheBus.js
+++ src/peopleOnTheBus.js
@@ -1,4 +1,4 @@
 const peopleOnTheBus = (busStops) => {
-    return busStops.length > 1 ? busStops[0][0] + busStops[1][0] : busStops[0][0];
+    return busStops.length > 1 ? busStops[0][0] + busStops[1][0] - busStops[1][1] : busStops[0][0];
 };
 module.exports = peopleOnTheBus;
